ced11343656fe0c65d86323e5d60ed05abd4b2ed
Nitish Kumar Yadav <nitish.kumar.yadav@rungta.org>
2026-03-02 16:40:40 +0530

    Add files via upload

---
+++ Voting.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Voting{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age:");
+        int age=sc.nextInt();
+        if(age>=18)
+            System.out.println("Eligible");
+        else
+            System.out.println("Not Eligible");
+    }
+}
+
+
+
+
+
+
+
+
+public class Voting{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age:");
+        int age=sc.nextInt();
+        if(age>=18)
+            System.out.println("Eligible");
+        else
+            System.out.println("Not Eligible");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+public class Voting{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age:");
+        int age=sc.nextInt();
+        if(age>=18)
+            System.out.println("Eligible");
+        else
+            System.out.println("Not Eligible");
+    }
+}
